5f6978c96f3b2b37cffbc683fbfcee4e7489b0e2
Heron Feijao <heronfeijao@gmail.com>
2022-04-12 21:39:39 -0300

    finalized cats

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,20 +2,20 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, cb) {
-  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
-
+    if (error) cb(undefined);
     //Log (cb function) is waiting to be called when readFile is done reading the file.
     if (!error) cb(data);
   });
 };
 
 //Use this function to ONLY return the value after the file has been read.
-const returnValue = (breed) => {
-  console.log('Return Value: ', breed);
-};
+// const returnValue = (breed) => {
+// console.log('Return Value: ', breed);
+// };
 
-console.log(returnValue);
+// console.log(returnValue);
 
-breedDetailsFromFile('Bombay', returnValue);
+// breedDetailsFromFile('Bombay', returnValue);
+
+module.exports = breedDetailsFromFile;
